0a1146871e6e5d937756336ba91ad55994ece053
Nathan <nathan.agez@epitech.eu>
2022-04-03 18:01:06 +0200

    feat(get-tasks): add get-tasks lambda function

---
+++ serverless-todo-rekognition/src/lambda/get-tasks/index.ts
@@ -0,0 +1,38 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb'
+import { DynamoDBDocumentClient, QueryCommand } from '@aws-sdk/lib-dynamodb'
+const ddbClient = new DynamoDBClient({
+    region: process.env.REGION
+})
+const ddbDocClient = DynamoDBDocumentClient.from(ddbClient)
+const tableName = process.env.TASKS_TABLE
+
+exports.handler = async (event: any) => {
+    console.info('received:', event)
+
+    const user = event.requestContext.authorizer.principalId
+    const params = {
+        TableName: tableName,
+        KeyConditionExpression: '#u = :u',
+        ExpressionAttributeNames: {
+            '#u': 'user'
+        },
+        ExpressionAttributeValues: {
+            ':u': `user#${user}`
+        }
+    }
+
+    console.info(`Querying table ${tableName}`)
+    const data = await ddbDocClient.send(new QueryCommand(params))
+    console.log('Success. Item details: ', data.Items)
+
+    const response = {
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': '*'
+        },
+        body: JSON.stringify(data.Items)
+    }
+
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`)
+    return response
+}
